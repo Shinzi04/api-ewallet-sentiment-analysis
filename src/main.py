f78f7cdd65895b2a993af9f7145a82038b5b155e
Shinzi <shinzisaja@gmail.com>
2025-12-01 14:38:48 +0700

    test_api_2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 # ROUTERS
 from .routes import model
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(model.router, prefix="/model", tags=["model"])
 
